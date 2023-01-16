8488b6d4ef3ab2b0939bd4aead176afdaed8a36b
bohdan.dovbysh <bohdan.dovbysh@cyberproof.com>
2023-01-16 21:13:23 +0200

    lesson_2

---
+++ list_functions.py
@@ -0,0 +1,34 @@
+my_list = [999, 25, 75, 1, 45, 1]
+
+# print(my_list.index(45))
+
+# my_list.append(75)
+# my_list.append(99)
+# print(my_list)
+# my_list.remove(45)
+# print(my_list)
+
+# my_list.sort(reverse=True)
+# print(my_list)
+
+# my_list.reverse()
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+# my_list_2 = my_list.copy()
+
+# print(my_list.count(55))
+
+# my_list_2 = [499999999999999, 5, 56]
+# my_list.extend(my_list_2)
+# # final_list = my_list + my_list_2
+# print(my_list)
+
+# my_list.insert(2, 'HEllo')
+# print(my_list)
+
+my_list.pop(2)
+print(my_list)
+
